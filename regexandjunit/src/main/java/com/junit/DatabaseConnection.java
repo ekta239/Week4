ef1d4998a80b565ec96c9942ddce51b94cc02c21
Ekta verma <ektav8231@gmail.com>
2025-02-18 22:30:23 +0530

    [Ekta] Add.Testing BeforeEach and AfterEach annotations

---
+++ regexandjunit/src/main/java/com/junit/DatabaseConnection.java
@@ -0,0 +1,11 @@
+package com.junit;
+
+public class DatabaseConnection {
+    public void connect() {
+        System.out.println("Connected to database");
+    }
+
+    public void disconnect() {
+        System.out.println("Disconnected from database");
+    }
+}
