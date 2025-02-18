b2e350d0565b0151d63e44fcc07127e4cb5001b8
Ekta verma <ektav8231@gmail.com>
2025-02-18 13:08:07 +0530

    [Ekta] Add. find Nth element from end ingiven singlyLinkedList without calculating its size

---
+++ collection/src/main/java/com/listinterface/ListNode.java
@@ -0,0 +1,12 @@
+package com.listinterface;
+
+public class ListNode {
+    public char value;
+   public ListNode next;
+
+    // Constructor
+    public ListNode(char value) {
+        this.value = value;
+        this.next = null;
+    }
+}
