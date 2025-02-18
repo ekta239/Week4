87733845f31d5ca9dde3ddba42df535e1705f73c
Ekta verma <ektav8231@gmail.com>
2025-02-18 13:11:20 +0530

    [Ekta] Add. Compute union and intersection of given two sets

---
+++ collection/src/main/java/com/setinterface/UnionIntersection.java
@@ -0,0 +1,27 @@
+package com.setinterface;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UnionIntersection {
+    public static <T> Set<T> union(Set<T> set1, Set<T> set2) {
+        Set<T> result = new HashSet<>(set1);
+        result.addAll(set2);
+        return result;
+    }
+
+    public static <T> Set<T> intersection(Set<T> set1, Set<T> set2) {
+        Set<T> result = new HashSet<>(set1);
+        result.retainAll(set2);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = Set.of(1, 2, 3);
+        Set<Integer> set2 = Set.of(3, 4, 5);
+
+        System.out.println("Union: " + union(set1, set2)); // Output: {1, 2, 3, 4, 5}
+        System.out.println("Intersection: " + intersection(set1, set2)); // Output: {3}
+    }
+}
