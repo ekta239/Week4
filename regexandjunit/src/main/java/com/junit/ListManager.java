c71eb28367452ee0490fd68ecf4a8ecd17a897ad
Ekta verma <ektav8231@gmail.com>
2025-02-18 22:28:30 +0530

    [Ekta] Add. Testing List Operations

---
+++ regexandjunit/src/main/java/com/junit/ListManager.java
@@ -0,0 +1,16 @@
+package com.junit;
+import java.util.List;
+
+public class ListManager {
+    public static void addElement(List<Integer> list, int element) {
+        list.add(element);
+    }
+
+    public static void removeElement(List<Integer> list, int element) {
+        list.remove(Integer.valueOf(element));
+    }
+
+    public static int getSize(List<Integer> list) {
+        return list.size();
+    }
+}
