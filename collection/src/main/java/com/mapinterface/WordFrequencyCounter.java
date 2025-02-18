9e69af69052ff1d3433e0ac87f0d664dc0666122
Ekta verma <ektav8231@gmail.com>
2025-02-18 13:22:41 +0530

    [Ekta] Add. Read text file and count frequency of each word using hashmap

---
+++ collection/src/main/java/com/mapinterface/WordFrequencyCounter.java
@@ -0,0 +1,32 @@
+package com.mapinterface;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordFrequencyCounter {
+    public static Map<String, Integer> countWordFrequency(String filePath) {
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countWordFrequency("sample.txt"));
+    }
+}
