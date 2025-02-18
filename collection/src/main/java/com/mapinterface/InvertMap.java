0aad60c4b81fc52b28447742554b1987ac9b12c8
Ekta verma <ektav8231@gmail.com>
2025-02-18 18:23:18 +0530

    [Ekta] Add. Handle duplicate values by storing them in list

---
+++ collection/src/main/java/com/mapinterface/InvertMap.java
@@ -0,0 +1,25 @@
+package com.mapinterface;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
+public class InvertMap {
+    public static <K, V> Map<V, List<K>> invertMap(Map<K, V> map) {
+        Map<V, List<K>> invertedMap = new HashMap<>();
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            invertedMap.computeIfAbsent(entry.getValue(), k -> new ArrayList<>()).add(entry.getKey());
+        }
+        return invertedMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 1);
+        System.out.println(invertMap(map)); // Output: {1=[A, C], 2=[B]}
+    }
+}
