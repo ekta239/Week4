b2de9f70dc31fa2e934b8ba4eee90bbc37bb1d3c
Ekta verma <ektav8231@gmail.com>
2025-02-18 13:12:59 +0530

    [Ekta] Add. Check elements present in either set but not in both of given sets

---
+++ collection/src/main/java/com/setinterface/SymmetricDifference.java
@@ -0,0 +1,22 @@
+package com.setinterface;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SymmetricDifference {
+    public static <T> Set<T> symmetricDifference(Set<T> set1, Set<T> set2) {
+        Set<T> result = new HashSet<>(set1);
+        result.addAll(set2);
+        Set<T> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        result.removeAll(intersection);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = Set.of(1, 2, 3);
+        Set<Integer> set2 = Set.of(3, 4, 5);
+        System.out.println(symmetricDifference(set1, set2)); // Output: {1, 2, 4, 5}
+    }
+}
